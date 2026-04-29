f3bcf8e1f4967271decc1a4e9a629c9a4ba0f92a
Samuel Williams <samuel.williams@shopify.com>
2026-04-29 09:44:59 +0900

    rubocop -A

--- test/async/webdriver/printing.rb
+++ test/async/webdriver/printing.rb
@@ -12,7 +12,7 @@ require "pool_context"
 APrinting = Sus::Shared("printing") do
 	include Sus::Fixtures::Async::ReactorContext
 	include Sus::Fixtures::Async::HTTP::ServerContext
-
+	
 	let(:app) do
 		proc do |request|
 			Protocol::HTTP::Response[200, [], [<<~HTML]]
@@ -39,64 +39,64 @@ APrinting = Sus::Shared("printing") do
 			HTML
 		end
 	end
-
+	
 	with "#print" do
 		it "returns valid PDF binary data" do
 			session.visit(bound_url)
-
+			
 			pdf_data = session.print
-
+			
 			expect(pdf_data).to be_a(String)
 			expect(pdf_data.length).to be > 0
 			# PDF files begin with the %PDF magic bytes
 			expect(pdf_data[0, 4]).to be == "%PDF"
 		end
-
+		
 		it "accepts orientation option" do
 			session.visit(bound_url)
-
+			
 			portrait  = session.print(orientation: "portrait")
 			landscape = session.print(orientation: "landscape")
-
+			
 			expect(portrait).to be_a(String)
 			expect(portrait[0, 4]).to be == "%PDF"
 			expect(landscape).to be_a(String)
 			expect(landscape[0, 4]).to be == "%PDF"
 		end
-
+		
 		it "accepts page_ranges option" do
 			session.visit(bound_url)
-
+			
 			pdf_data = session.print(page_ranges: ["1"])
-
+			
 			expect(pdf_data).to be_a(String)
 			expect(pdf_data[0, 4]).to be == "%PDF"
 		end
-
+		
 		it "accepts scale option" do
 			session.visit(bound_url)
-
+			
 			pdf_data = session.print(scale: 0.5)
-
+			
 			expect(pdf_data).to be_a(String)
 			expect(pdf_data[0, 4]).to be == "%PDF"
 		end
-
+		
 		it "accepts margin option" do
 			session.visit(bound_url)
-
+			
 			pdf_data = session.print(margin: {top: 2, bottom: 2, left: 2, right: 2})
-
+			
 			expect(pdf_data).to be_a(String)
 			expect(pdf_data[0, 4]).to be == "%PDF"
 		end
-
+		
 		it "accepts page size option" do
 			session.visit(bound_url)
-
+			
 			# A4 dimensions in cm
 			pdf_data = session.print(page: {width: 21.0, height: 29.7})
-
+			
 			expect(pdf_data).to be_a(String)
 			expect(pdf_data[0, 4]).to be == "%PDF"
 		end
@@ -105,21 +105,21 @@ end
 
 Async::WebDriver::Bridge.each do |klass|
 	name = klass.name.split("::").last
-
+	
 	describe(klass, unique: name) do
 		include PoolContext
-
+		
 		if klass <= Async::WebDriver::Bridge::Safari
 			include Sus::Fixtures::Async::ReactorContext
 			include Sus::Fixtures::Async::HTTP::ServerContext
-
+			
 			let(:app) do
-				proc { |request| Protocol::HTTP::Response[200, [], ["<html><body></body></html>"]] }
+				proc {|request| Protocol::HTTP::Response[200, [], ["<html><body></body></html>"]]}
 			end
-
+			
 			it "raises UnknownCommandError (Safari does not support the print endpoint)" do
 				session.visit(bound_url)
-				expect { session.print }.to raise_exception(Async::WebDriver::UnknownCommandError)
+				expect {session.print}.to raise_exception(Async::WebDriver::UnknownCommandError)
 			end
 		else
 			it_behaves_like APrinting
--- test/async/webdriver/window.rb
+++ test/async/webdriver/window.rb
@@ -12,19 +12,19 @@ require "pool_context"
 AWindow = Sus::Shared("window") do
 	include Sus::Fixtures::Async::ReactorContext
 	include Sus::Fixtures::Async::HTTP::ServerContext
-
+	
 	let(:app) do
 		proc do |request|
 			Protocol::HTTP::Response[200, [], ["<html><body><h1>Window Test</h1></body></html>"]]
 		end
 	end
-
+	
 	with "#window_rect" do
 		it "returns a hash with x, y, width, and height keys" do
 			session.visit(bound_url)
-
+			
 			rect = session.window_rect
-
+			
 			expect(rect).to be_a(Hash)
 			expect(rect["width"]).to be_a(Numeric)
 			expect(rect["height"]).to be_a(Numeric)
@@ -32,68 +32,68 @@ AWindow = Sus::Shared("window") do
 			expect(rect["y"]).to be_a(Numeric)
 		end
 	end
-
+	
 	with "#resize_window" do
 		it "changes the window dimensions" do
 			session.visit(bound_url)
-
+			
 			session.resize_window(1024, 768)
 			rect = session.window_rect
-
+			
 			expect(rect["width"]).to be == 1024
 			expect(rect["height"]).to be == 768
 		end
-
+		
 		it "can be resized multiple times" do
 			session.visit(bound_url)
-
+			
 			session.resize_window(800, 600)
 			rect = session.window_rect
 			expect(rect["width"]).to be == 800
 			expect(rect["height"]).to be == 600
-
+			
 			session.resize_window(1280, 900)
 			rect = session.window_rect
 			expect(rect["width"]).to be == 1280
 			expect(rect["height"]).to be == 900
 		end
 	end
-
+	
 	with "#set_window_rect" do
 		it "accepts width and height keyword arguments" do
 			session.visit(bound_url)
-
+			
 			session.set_window_rect(width: 900, height: 700)
 			rect = session.window_rect
-
+			
 			expect(rect["width"]).to be == 900
 			expect(rect["height"]).to be == 700
 		end
 	end
-
+	
 	with "#maximize_window" do
 		it "maximizes the window" do
 			session.visit(bound_url)
-
+			
 			# Shrink first so maximise has something to do
 			session.resize_window(400, 300)
-
+			
 			session.maximize_window
 			rect = session.window_rect
-
+			
 			expect(rect["width"]).to be > 400
 			expect(rect["height"]).to be > 300
 		end
 	end
-
+	
 	with "#fullscreen_window" do
 		it "makes the window fullscreen" do
 			session.visit(bound_url)
-
+			
 			session.resize_window(800, 600)
 			session.fullscreen_window
 			rect = session.window_rect
-
+			
 			# Fullscreen dimensions should be at least as large as the pre-fullscreen size
 			expect(rect["width"]).to be >= 800
 			expect(rect["height"]).to be >= 600
@@ -104,10 +104,10 @@ end
 Async::WebDriver::Bridge.each do |klass|
 	name = klass.name.split("::").last
 	pool = Async::WebDriver::Bridge::Pool.new(klass.new)
-
+	
 	describe(klass, unique: name) do
 		include PoolContext
-
+		
 		it_behaves_like AWindow
 	end
 end
