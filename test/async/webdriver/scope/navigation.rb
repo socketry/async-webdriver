02651300c0df8e98dff85cb38378496d993a18ef
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2026-04-29 10:55:40 +0900

    Modernize code.

--- test/async/webdriver/scope/navigation.rb
+++ test/async/webdriver/scope/navigation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2023-2025, by Samuel Williams.
+# Copyright, 2025-2026, by Samuel Williams.
 
 require "sus/fixtures/async/reactor_context"
 require "sus/fixtures/async/http/server_context"
